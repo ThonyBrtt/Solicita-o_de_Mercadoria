4e56a9e46489a1dc9698c26d6bd59f69ec3700a9
Mateus Cerqueira Correia <mateuscorreia14@gmail.com>
2026-05-12 11:13:10 -0300

    upd Alteração de ligações do database

--- backend/database.py
+++ backend/database.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 def get_conn():
     return psycopg2.connect(
-        host="db.rqjdeorqsrfqgugmncnm.supabase.co",
+        host="aws-1-sa-east-1.pooler.supabase.com",
         database="postgres",
-        user="postgres",
+        user="postgres.rqjdeorqsrfqgugmncnm",
         password=os.getenv("DB_PASSWORD"),
         port=5432
     )
